9966e5317bdc5fa12c054e46e7f536f312dfb78d
L0wK11 <110197109+L0wK11@users.noreply.github.com>
2024-05-03 08:55:11 +0800

    upl node modules

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-xl bg-slate-300 text-left'>Hello Tailwind CSS with react vite</h1>
+      <h1 className='text-xl bg-slate-300 text-left'>Hello Tailwind CSS with React vite</h1>
     </>
   )
 }
