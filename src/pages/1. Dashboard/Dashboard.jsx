6bd5b98c1f8c2d05d844b8881b4f53350264bdf0
L0wK11 <110197109+L0wK11@users.noreply.github.com>
2024-05-08 20:05:21 +0800

    plus points

--- src/pages/1. Dashboard/Dashboard.jsx
+++ src/pages/1. Dashboard/Dashboard.jsx
@@ -3,8 +3,14 @@ const Dashboard = () => {
         <main className="bg-slate-200  w-full">
             <div className=" text-center font-bold">
                 <h1 className="text-[20px] text-center">Home DashBoard</h1>
-                <h2>Welcome, User!</h2>
-                <p>Welcome to the dashboard! This is the main content area where you can display various information and charts.</p>
+                <h2>Welcome, Maam!! PLUSS POINTSS NA DISSS</h2>
+                <img className=" ml-[300px]"
+            src="https://scontent.fmnl13-2.fna.fbcdn.net/v/t39.30808-6/432758749_1212479286383722_8709524054415877154_n.jpg?stp=cp6_dst-jpg&_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEb1TJuVxkclikMJ5z0TGW1h-p_vMiC3JWH6n-8yILclbgLDYyBatn5kbjmZlGztIWhm6aOF9b5oFC0sqtMYnCg&_nc_ohc=xpl27W0l2XwQ7kNvgEbWlAT&_nc_ht=scontent.fmnl13-2.fna&oh=00_AfBrgKIcu0APFXJCWz32PTtZ5BwyLyOl5t4JgeqMP23L4Q&oe=66414545"
+            alt="Student Image"
+            width="950"
+        
+            />
+                
             </div>
         </main>
     );
